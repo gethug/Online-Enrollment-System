91e29f785100674f58f61fb5edb1e2b903d29b60
gethug <hermosagens@yahoo.com>
2014-08-29 08:01:19 -0700

    fake

--- laravel/app/views/server/level/level.blade.php
+++ laravel/app/views/server/level/level.blade.php
@@ -40,52 +40,6 @@
 
     </tbody>
 </table>
-
-<script>
-    $(function() {
-      // Add custom class to pagination div
-          $.fn.dataTableExt.oStdClasses.sPaging = 'dataTables_paginate paging_bootstrap paging_custom';
-
-          var oTable02 = $('#inlineEditDataTable').dataTable({
-            "sDom":
-              "R<'row'<'col-md-6'l><'col-md-6'f>r>"+
-              "t"+
-              "<'row'<'col-md-4 sm-center'i><'col-md-4'><'col-md-4 text-right sm-center'p>>",
-            "oLanguage": {
-              "sSearch": ""
-            },
-            "aaSorting": [[ 0, "desc" ]],
-            "aoColumnDefs": [
-              {   'bSortable': false, 
-                'aTargets': [ "no-sort" ] ,
-              }
-            ],
-            "fnInitComplete": function(oSettings, json) {
-              $('.dataTables_filter input').attr("placeholder", "Search");
-            }
-          });
-
-          // hide first column
-          oTable02.fnSetColumnVis(0, false);
-
-          // append add row button to table
-          var addRowLink = '<a href="#" id="addRow" class="btn btn-green btn-xs add-row">Print</a>'
-          $('#inlineEditDataTable_wrapper').append(addRowLink);
-
-          var nEditing = null;
-          
-          // delete function
-          $(document).on("click", "#inlineEditDataTable a.delete", function(e) {
-              var id = $(this).attr('id');
-              
-              $('input[name="delete-id"]').val(id);
-              $('form#delete').attr('action', 'candidate/' + id);
-          });
-
-          //initialize chosen
-          $('.dataTables_length select').chosen({disable_search_threshold: 10});
-    })
-  </script>
 </div>
 
 @stop
